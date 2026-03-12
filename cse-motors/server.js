b49ded78dbd35431c99d5866e0f5b85f9cd4396e
Omoregbe Oghodo <omoregbeoghodo54@gmail.com>
2026-03-12 16:50:12 +0100

    Update server.js

--- cse-motors/server.js
+++ cse-motors/server.js
@@ -1,26 +1,43 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
 const express = require("express")
-
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
 const app = express()
+const static = require("./routes/static")
 
-const PORT = process.env.PORT || 3000
-
-
-/* View Engine */
+/* ***********************
+ * View Engines and Templates 
+ *************************/
 app.set("view engine", "ejs")
-app.set("views", "./views")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
 
-/* Static Files */
-app.use(express.static("public"))
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
 
-/* Routes */
-const staticRoutes = require("./routes/static")
-
-app.use("/", staticRoutes)
-
-
-/* Server Listener */
-app.listen(PORT, function(){
-    console.log(`Server running on port ${PORT}`)
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
 })
